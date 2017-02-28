b6921cc70c5a5bcc5d4810ad5888cc6c5a4bb8ed
huynguyenptit <huynguyenptit97@gmail.com>
2017-02-28 09:19:39 +0700

    fix slide

--- jquery02.js
+++ jquery02.js
@@ -1,142 +1,150 @@
-var imgArr = new Array('img0', 'img1', 'img2');
-var cur = 0;
-var widthImg = 1000;
-var direction = "";
-var slide = 0;
-var time = null; 
-importSlide(imgArr);
-runSlide();
-function importSlide(arrayImg){
-for (var i = 0; i < arrayImg.length; i++) {
-	imgCenter = '<img src="img/'+ arrayImg[i] + '.jpg">';
-	imgLeft = '<img src="img/'+ arrayImg[getPrev(i)] + '.jpg">';
-	imgRight = '<img src="img/'+ arrayImg[getNext(i)] + '.jpg">';
-	$('#main-slider').append(imgCenter);
-	$('#imgleft').append(imgLeft);
-	$('#imgright').append(imgRight);
-	$('#choose').append('<li></li>');
-}
-}
-
-function getPrev(index){
-	var x = index - 1;
-	if (x <= 0) {
-		return 0;
+$(document).ready(function(){
+	var imgArr = new Array('img0', 'img1', 'img2');
+	var cur = 0;
+	var widthImg = 1000;
+	var direction = "";
+	var slide = 0;
+	 //import img into slide
+	for (var i = 0; i < imgArr.length; i++) {
+		imgCenter = '<img src="img/'+ imgArr[i] + '.jpg">';
+		imgLeft = '<img src="img/'+ imgArr[getPrev(i)] + '.jpg">';
+		imgRight = '<img src="img/'+ imgArr[getNext(i)] + '.jpg">';
+		$('#main-slider').append(imgCenter);
+		$('#imgleft').append(imgLeft);
+		$('#imgright').append(imgRight);
+		$('#choose').append('<li></li>');
 	}
-	return x;
-}
-
-function getNext(index){
-	var y = index + 1;
-	if (y >= imgArr.length) {
-		return 0;
-	}
-	return y;
-}
-
-function goLeft(){
-	cur = getPrev(cur);
-	goImg(cur);
-}
-
-function goRight(){
-	cur = getNext(cur);
-	goImg(cur);
-}
-
-function goImg(index){
-	$('#main-slider img').show();
-	$('#main-slider').css('margin-left',(-1) * index * widthImg+"px");
-	$('.select').removeClass('select');
-	$('#choose li').eq(cur).addClass('select');
-
-	//change boths side img
-	$('#imgleft img').hide();
-	$('#imgright img').hide();
-	$('#imgleft img').show();
-	$('#imgright img').show();
-}
-
-function runSlide(){
-	if (direction == "left") {
-		time = setInterval(function(){
-			goLeft();
-		}, 1500);
-	}
-	else{
-		time = setInterval(function(){
-			goRight();
-		}, 1500);
-	}
-}
-
-$('#prev').click(function(){
-	clearInterval(time);
-	goLeft();
-	direction = "left";
 	runSlide();
-});
+	function goImg(index){
+		$('#main-slider img').show();
+		$('#main-slider').css('margin-left',(-1) * index * widthImg+"px");
+		$('.select').removeClass('select');
+		$('#choose li').eq(cur).addClass('select');
 
-$('#next').click(function(){
-	clearInterval(time);
-	goRight();
-	direction = "right";
-	runSlide();
-});
-hover();
-function hover(){
-	$('#prev').mouseenter(function(){
-		// clearInterval(time);
-		$("#imgleft").show();
-	});
-	$('#next').mouseenter(function(){
-		// clearInterval(time);
-		$("#imgright").show();
-	});
-	$('#prev').mouseleave(function(){
-		$("#imgleft").hide();
-	});
-	$('#next').mouseleave(function(){
-		$("#imgright").hide();
-	});
-}
+		//change boths side img
+		$('#imgleft img').hide();
+		$('#imgright img').hide();
+		$('#imgleft img').eq(cur).show();
+		$('#imgright img').eq(cur).show();
+	}
 
+	// move left
+	function getPrev(index){
+		var x = index - 1;
+		if (x < 0) {
+			return imgArr.length-1 ;
+		}
+		return x;
+	}
+	function goLeft(){
+		cur = getPrev(cur);
+		goImg(cur);
+	}
 
+	//move right
+	function getNext(index){
+		var y = index + 1;
+		if (y >= imgArr.length) {
+			return 0;
+		}
+		return y;
+	}
+	function goRight(){
+		cur = getNext(cur);
+		goImg(cur);
+	}
 
-$("#choose li").click(function(){
-	cur = $("li").index($(this));
-	clearInterval(time);
-	goImg(cur);
-	runSlide();
-});
-
-$("#box").mouseenter(function(){
-	clearInterval(time);
-});
-
-$("#box").mouseleave(function(){
-	if (direction == "left") {
+	// set time to run slide
+	var time = null;
+	function runSlide(){
+		if (direction == "left") {
+			time = setInterval(function(){
+				goLeft();
+			}, 1500);
+		}
+		else{
+			time = setInterval(function(){
+				goRight();
+			}, 1500);
+		}
+	}
+	// see previous slide
+	$('#prev').click(function(){
+		clearInterval(time);
 		goLeft();
-	}
-	else{
-		goRight();
-	}
-	runSlide();
-});
+		direction = "left";
+		runSlide();
+	});
 
-$("#option").click(function(){
-	if ($("#check-thumbnail:checked").length) {
-		hover();
+	//see next slide
+	$('#next').click(function(){
+		clearInterval(time);
+		goRight();
+		direction = "right";
+		runSlide();
+	});
+
+	//hover thumnail to see next and prev img
+	hover();
+	function hover(){
+		$('#prev').mouseenter(function(){
+			// clearInterval(time);
+			$("#imgleft").show();
+			
+		});
+		$('#next').mouseenter(function(){
+			// clearInterval(time);
+			$("#imgright").show();
+		});
+		$('#prev').mouseleave(function(){
+			$("#imgleft").hide();
+		});
+		$('#next').mouseleave(function(){
+			$("#imgright").hide();
+		});
 	}
-	else{
-		$('#prev').unbind("mouseenter");
-		$('#next').unbind("mouseenter");
-	}
-	if ($("#check-dot:checked").length){
-		$('#choose').show();
-	}
-	else
-	{
-		$('#choose').hide();
-	}
+
+	//choose img when click on dot
+	$("#choose li").click(function(){
+		cur = $("li").index($(this));
+		clearInterval(time);
+		goImg(cur);
+		runSlide();
+	});
+
+	//stop run slide when hover into #box
+	$("#box").mouseenter(function(){
+		clearInterval(time);
+	});
+
+	//continue running slide
+	$("#box").mouseleave(function(){
+		// set up the direction
+		if (direction == "left") {
+			goLeft();
+		}
+		else{
+			goRight();
+		}
+		runSlide();
+	});
+
+	// set up the option of see dot and see next/prev img
+	$("#option").click(function(){
+		// check the checked attribute
+		if ($("#check-thumbnail:checked").length){
+			hover();
+		}
+		else{
+			$('#prev').unbind("mouseenter");
+			$('#next').unbind("mouseenter");
+		}
+		if ($("#check-dot:checked").length){
+			$('#choose').show();
+		}
+		else
+		{
+			$('#choose').hide();
+		}
+	});
 });
